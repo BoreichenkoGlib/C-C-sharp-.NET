552ef0fb38d7dbd3fb61a20cfaa11c9835e35235
BoreichenkoGlib <boreichenkoglib@gmail.com>
2021-08-06 18:30:45 +0300

    C/C#/.NET

---
+++ C#/10-C#.Math.Solving combinatorics problems (2)/lab32/Combinations/Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Combinations
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            labelNk.Visible = false;
+        }
+
+        private void ButtonResult_Click(object sender, EventArgs e)
+        {
+            int n, k;
+            n = (int)numericUpDownN.Value;
+            k = (int)numericUpDownK.Value;
+
+            int result = 0, up = 1, down = 1, down2 = 1;
+            string[] split;
+            int[] arr;
+            switch (comboBoxCombination.SelectedIndex)
+            {
+
+                case 0:
+
+                    pictureBoxFormula.Image = Combinations.Properties.Resources.NoRepeat;
+                     result = (int)Math.Pow(n, k);
+                    break;
+                case 1:
+
+                    pictureBoxFormula.Image = Combinations.Properties.Resources.Розміщення_без_повтореннь;
+                    down = 1;
+                    up = 1;
+                    for (int i = 1; i <= n; i++)
+                        up *= i;
+                    for (int i = 1; i <= n - k; i++)
+                        down *= i;
+
+                    result = up / down;
+                    break;
+                case 2:
+
+                    pictureBoxFormula.Image = Combinations.Properties.Resources.Сполучення_без_повтореннь;
+
+                    down = 1;
+                    up = 1;
+                    for (int i = 1; i <= n; i++)
+                        up *= i;
+                    for (int i = 1; i <= n - k; i++)
+                        down *= i;
+                    for (int i = 1; i <= k; i++)
+                        down2 *= i;
+
+                    result = up / (down * down2);
+                    break;
+                case 3:
+
+                    pictureBoxFormula.Image = Combinations.Properties.Resources.Сполучення_з_повтореннями;
+
+                    down = 1;
+                    up = 1;
+                    n += k - 1;
+                    for (int i = 1; i <= n; i++)
+                        up *= i;
+                    for (int i = 1; i <= n - k; i++)
+                        down *= i;
+                    for (int i = 1; i <= k; i++)
+                        down2 *= i;
+
+                    result = up / (down * down2);
+                    break;
+                case 4:
+                    pictureBoxFormula.Image = Combinations.Properties.Resources.Перестановки_без_повтореннь;
+                    down = 1;
+                    up = 1;
+                    for (int i = 1; i <= n; i++)
+                        up *= i;
+
+                    result = up;
+                    break;
+                case 5:
+                    pictureBoxFormula.Image = Combinations.Properties.Resources.Перестановки_з_повтореннями;
+
+                    for (int i = 1; i <= n; i++)
+                        up *= i;
+                    split = textBoxKArr.Text.Split(' ');
+                    arr = new int[split.Length];
+                    for (int i = 0; i < arr.Length; i++)
+                    {
+                        arr[i] = int.Parse(split[i]);
+                    }
+
+                    int tmp = 1;
+                    for (int i = 0; i < arr.Length; i++)
+                    {
+                        tmp = 1;
+                        for (int j = 1; j <= arr[i]; j++)
+                        {
+                            tmp *= j;
+                        }
+                        arr[i] = tmp;
+                    }
+                    down = 1;
+                    up = 1;
+                    for (int i = 0; i < arr.Length; i++)
+                        down *= arr[i];
+
+                    for (int i = 1; i <= n; i++)
+                        up *= i;
+
+                    result = up / down;
+                    break;
+            }
+            textBoxResult.Text = result.ToString();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            numericUpDownK.Visible = true;
+            textBoxKArr.Visible = false;
+        }
+
+        private void ComboBoxCombination_SelectionChangeCommitted(object sender, EventArgs e)
+        {
+            if (comboBoxCombination.SelectedIndex == 5)
+            {
+                numericUpDownK.Visible = false;
+                textBoxKArr.Visible = true;
+                labelNk.Visible = true;
+            }
+            else if (comboBoxCombination.SelectedIndex == 4)
+            {
+                numericUpDownK.Visible = false;
+                textBoxKArr.Visible = false;
+                labelK.Visible = false;
+                labelNk.Visible = false;
+            }
+            else
+            {
+                numericUpDownK.Visible = true;
+                textBoxKArr.Visible = false;
+                labelK.Visible = true;
+                labelNk.Visible = false;
+            }
+        }
+    }
+}
---
+++ C#/11-C#.Math.finding the frame tree of the graph of minimum weight according to the algorithm of Prima and Kraskal/ConsoleApp1/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleApplication4
+{
+    public class Program
+    {
+        
+
+        struct edge_t
+        {
+            public int x, y;
+            public int w;               
+        }
+        static int[] nodes = new int[100];
+        static int last_n = 0;
+
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.Unicode;
+            edge_t[] edges = new edge_t[100];
+            int NV;             
+            int NE;           
+            int i;
+            //Console.ForegroundColor = ConsoleColor.Blue;
+            //Console.BackgroundColor = ConsoleColor.Gray;
+            Console.WriteLine("START");
+            Console.ResetColor();
+            Console.WriteLine("_____________________________");
+            //Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write("Кількість вершин-> ");
+            NV = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Кількість ребер-> ");
+            NE = Convert.ToInt32(Console.ReadLine());
+            //Console.ForegroundColor = ConsoleColor.Yellow;
+
+            for (i = 0; i < NV; i++)
+            {
+                nodes[i] = -1 - i;
+            }
+            Console.WriteLine("Матриця вершин та ребер:");
+            for (i = 0; i < NE; i++)
+            {
+                Console.Write("x" + (i + 1) + "-> ");
+                edges[i].x = Convert.ToInt32(Console.ReadLine());
+                Console.Write("y" + (i + 1) + "-> ");
+                edges[i].y = Convert.ToInt32(Console.ReadLine());
+                Console.Write("w" + (i + 1) + "-> ");
+                edges[i].w = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("----->");
+
+            }
+            //Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Мінімальний шлях: ");
+            int sum = 0;
+            var sVes = from h in edges orderby h.w select h;
+            foreach (var s in sVes)
+            {
+                for (i = 0; i < NE; i++)
+                {
+                    int c = getColor(s.y);
+                    if (getColor(s.x) != c)
+                    {
+                        nodes[last_n] = s.y;
+                        Console.Write("-> ("+s.x + ";" + s.y+")" + "=" + s.w);
+                        Console.WriteLine("");
+                        sum += s.w;
+                    }
+                }
+            }
+            Console.WriteLine("Мінімальна сума шляхів: "+sum);
+            //Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("_____________________________");
+            //Console.BackgroundColor = ConsoleColor.Gray;
+            Console.WriteLine("START");
+            Console.ReadLine();
+        }
+
+        static public int getColor(int n)
+        {
+            int c;
+            if (nodes[n] < 0)
+            {
+                return nodes[last_n = n];
+            }
+            c = getColor(nodes[n]);
+            nodes[n] = last_n;
+            return c;
+        }
+    }
+}
